0cd5285795f540b7657803ba75cfbfed9935ce18
R M Shahidul Islam Shahed <shahed.mbstu@gmail.com>
2017-10-15 00:21:27 +0600

    + Project code added

---
+++ Sln.CsharpDelegatesExample/CsharpDelegatesExample/CommonMethods.cs
@@ -0,0 +1,28 @@
+﻿namespace CsharpDelegatesExample
+{
+    public class CommonMethods
+    {
+        public int AdditionNum(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int SubtractionNum(int x, int y)
+        {
+            if (y > x) return y - x;
+            return x - y;
+        }
+
+        public int MultiplicationNum(int x, int y)
+        {
+            return x * y;
+        }
+        
+        public int DivisionNum(int x, int y)
+        {
+            if (y > x) return y / x;
+            return x / y;
+        }
+
+    }
+}
---
+++ Sln.CsharpDelegatesExample/CsharpDelegatesExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+delegate T CommonMathOperationDelegates<T>(T Num1, T Num2);
+
+namespace CsharpDelegatesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommonMethods oCommonMethods = new CommonMethods();
+
+            CommonMathOperationDelegates<int> Addition = new CommonMathOperationDelegates<int>(oCommonMethods.AdditionNum);
+            CommonMathOperationDelegates<int> Subtraction = new CommonMathOperationDelegates<int>(oCommonMethods.SubtractionNum);
+            CommonMathOperationDelegates<int> Multiplication = new CommonMathOperationDelegates<int>(oCommonMethods.MultiplicationNum);
+            CommonMathOperationDelegates<int> Division = new CommonMathOperationDelegates<int>(oCommonMethods.DivisionNum);
+
+            Console.Write("Enter First Number: ");
+            int X = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("Enter Second Number: ");
+            int Y = Convert.ToInt16(Console.ReadLine());
+
+
+            Console.WriteLine("*****************************************");
+            Console.WriteLine("Addition: " + Addition(X, Y));
+            Console.WriteLine("Subtraction: " + Subtraction(X, Y));
+            Console.WriteLine("Multiplication: " + Multiplication(X, Y));
+            Console.WriteLine("Division: " + Division(X, Y));
+
+            Console.ReadLine();
+        }
+    }
+}
